9a1bdfed814dd0b076bcce3689d190223b6f4afa
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-11-03 23:14:35 +0300

    layout, header, ui components

---
+++ src/app/containers/baselayout/baselayout.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-baselayout',
+  templateUrl: './baselayout.component.html',
+  styleUrls: ['./baselayout.component.sass'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BaselayoutComponent {
+
+}
